1f411182d1590b857e96963931343040396006ed
Miloslav Žežulka <zezulka@protonmail.com>
2022-02-06 12:09:09 +0100

    add Quarkus project skeleton

---
+++ src/native-test/java/cz/demo/NativeGreetingControllerIT.java
@@ -0,0 +1,9 @@
+package cz.demo;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingControllerIT extends GreetingControllerTest {
+
+    // Execute the same tests but in native mode.
+}
